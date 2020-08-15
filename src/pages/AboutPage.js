957173d6a23da40bccb13f097d68179a00f6eb75
Glenn <57659894+dev-glennn@users.noreply.github.com>
2020-08-15 20:40:07 +0900

    feat: 태그영역 1차 마무리

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Template from "../components/common/Template";
 import Introduce from "../components/about/Introduce";
+import Tags from "../components/about/Tags";
 
 const AboutPage = () => {
     return (
@@ -15,14 +16,19 @@ const AboutPage = () => {
                 </div>
                 <div className="col-5">
                     <h2>
-                        <img src="/about/introduce.svg" className="img-sm" alt="Introduce Icon"/>
+                        <img src="/about/tag.svg" className="img-sm" alt="Tags Icon"/>
                         tags
                     </h2>
-                    1
+                    <Tags/>
                 </div>
             </div>
             <div className="row">
-                2${}
+                <div className="col-6">
+                    <h2>Front - End</h2>
+                </div>
+                <div className="col-6">
+                    <h2>Back - End</h2>
+                </div>
             </div>
             <div className="row last-row">
                 3
