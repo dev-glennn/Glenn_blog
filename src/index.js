a05fa0d3b57581e9d170b6c997c32ce507e45f27
Glenn <57659894+dev-glennn@users.noreply.github.com>
2020-09-07 14:46:58 +0900

    style: basename 추가

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
-// import {Provider} from "react-redux";
-// import {createStore} from 'redux';
-// import {composeWithDevTools} from 'redux-devtools-extension';
-// import rootReducer from './modules';
-
-// const store = createStore(rootReducer, composeWithDevTools(),);
 
 ReactDOM.render(
-    // <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App/>
     </BrowserRouter>
-    // </Provider>
     , document.getElementById('root')
 );
 
